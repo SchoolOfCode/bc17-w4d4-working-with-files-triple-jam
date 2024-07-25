e63973df1c4029d0bcef413b049f99a90c4b7a93
Isuu1 <jakub.babelek@gmail.com>
2024-07-25 13:35:58 +0100

    Resolving Ticket 2b

--- quote.js
+++ quote.js
@@ -1,24 +1,23 @@
-import fs from "node:fs/promises";
+import fs, { readFile } from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
 
 export async function getQuotes() {
-
+  try {
+    const data = await readFile(fileName, "utf8");
+    console.log("Data: " + data);
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export async function addQuote(quoteText) {
+export async function addQuote(quoteText) {}
 
-}
+export async function getRandomQuote() {}
 
-export async function getRandomQuote() {
+export async function editQuote(id, quoteText) {}
 
-}
-
-export async function editQuote(id, quoteText) {
-
-}
-
-export async function deleteQuote(id) {
-
-}
+export async function deleteQuote(id) {}
